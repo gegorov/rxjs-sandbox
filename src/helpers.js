9bd8b68e17e73bfe6699a01c0117dd11ed03aa4a
gegorov <gegorov@yahoo.com>
2019-09-02 15:01:48 +0300

    refactor app based on PR comments

--- src/helpers.js
+++ src/helpers.js
@@ -1,31 +1,39 @@
 import { of } from 'rxjs';
 
+import { API_URL, ERROR_CSS_CLASS } from './constants';
+
+const token = process.env.GITHUB_TOKEN;
+
+
 export const addItem = (node, item, error) => {
-  const li = document.createElement('li');
-  const textNode = document.createTextNode(item);
-  li.appendChild(textNode);
+    const li = document.createElement('li');
+    const textNode = document.createTextNode(item);
+    li.appendChild(textNode);
 
-  if (error) {
-    li.setAttribute('style', 'color:red');
-  }
+    if (error) {
+        li.classList.add(ERROR_CSS_CLASS);
+    }
 
-  node.appendChild(li);
+    node.appendChild(li);
 };
 
 export const clearItems = (root) => {
-  while (root.hasChildNodes()) {
-    root.removeChild(root.lastChild);
-  }
+    while (root.hasChildNodes()) {
+        root.removeChild(root.lastChild);
+    }
 };
 
 export const handleWrongQueries = (query, root) => {
-  if (!query) {
+    if (query.length > 255) {
+        addItem(root, 'Query is too long:( Try something shorter.', true);
+        return of({ error: true, message: 'empty input' });
+    }
+    return null;
+};
+
+export const handleEmptyQuery = (root) => {
     addItem(root, 'Try to type in somenthing.', true);
     return of({ error: true, message: 'empty input' });
-  }
-  if (query.length > 255) {
-    addItem(root, 'Query is too long:( Try something shorter.', true);
-    return of({ error: true, message: 'empty input' });
-  }
-  return null;
 };
+
+export const urlBuilder = (query) => `${API_URL}/search/users?q=${query}+in:login&access_token=${token}`;
--- src/index.js
+++ src/index.js
@@ -1,51 +1,58 @@
 import { of, fromEvent } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
 import {
-  switchMap,
-  catchError,
-  tap,
-  map,
-  debounceTime,
-  distinctUntilChanged,
+    switchMap,
+    catchError,
+    tap,
+    map,
+    debounceTime,
+    distinctUntilChanged,
 } from 'rxjs/operators';
 
-import { addItem, clearItems, handleWrongQueries } from './helpers';
-import { API_URL } from './constants';
+import {
+    addItem,
+    clearItems,
+    handleWrongQueries,
+    handleEmptyQuery,
+    urlBuilder,
+} from './helpers';
+import { DEBOUNCING_TIME } from './constants';
 
-const token = process.env.GITHUB_TOKEN;
-
+import './styles/styles.css';
 
 const root = document.getElementById('root');
 const searchInput = document.getElementById('searchInput');
 
 const fetchData = (query) => {
-  handleWrongQueries(query, root);
-  return fromFetch(`${API_URL}/search/users?q=${query}+in:login&access_token=${token}`)
-    .pipe(
-      switchMap((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        return of({ error: true, message: `Error ${response.status}` });
-      }),
-      catchError((err) => of({ error: true, message: err.message })),
-    );
+    if (!query) {
+        return handleEmptyQuery(root);
+    }
+    handleWrongQueries(query, root);
+    return fromFetch(urlBuilder(query))
+        .pipe(
+            switchMap((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                return of({ error: true, message: `Error ${response.status}` });
+            }),
+            catchError((err) => of({ error: true, message: err.message })),
+        );
 };
 
 
 const stream$ = fromEvent(searchInput, 'input')
-  .pipe(
-    tap(() => clearItems(root)),
-    debounceTime(200),
-    map((event) => event.target.value),
-    debounceTime(200),
-    distinctUntilChanged(),
-    switchMap(fetchData),
-    catchError((err) => of({ error: true, message: err.message })),
-    tap(({ items = [] }) => {
-      items.map(({ login, id }) => addItem(root, `login: ${login}, id: ${id}`));
-    }),
-  );
+    .pipe(
+        tap(() => clearItems(root)),
+        debounceTime(DEBOUNCING_TIME),
+        map((event) => event.target.value),
+        distinctUntilChanged(),
+        switchMap(fetchData),
+        catchError((err) => of({ error: true, message: err.message })),
+        tap(({ items = [] }) => {
+            items.map(({ login, id }) => addItem(root, `login: ${login}, id: ${id}`));
+        }),
+    );
 
 
 stream$.subscribe();
